f524bc16cf2d0a2c161f31dffb9f50dd51bcfe7c
Samitha Basnayake <36471900+bsamitha@users.noreply.github.com>
2025-03-05 09:57:21 +0530

    added developer note for deleteVMs.ps1

--- deleteVMs.ps1
+++ deleteVMs.ps1
@@ -1,6 +1,17 @@
+<#
+-------------------Cloud Resource Cleanup Automation-------------------
+Identifies and deletes unused Azure VMs older than 30 days
+Generates a report of deleted VMs for auditing
+
+-------------------NotTested-------------------
+#>
+
 # List VMs older than 30 days
+# cutoff date for last 30 days
 $cutoffDate = (Get-Date).AddDays(-30)
 
+# using azure cli
+# Install Azure CLI and az login for authnticate
 $oldVMs = az vm list --query "[?timeCreated<'$($cutoffDate.ToString("yyyy-MM-ddTHH:mm:ssZ"))']" -o json | ConvertFrom-Json
 
 # Delete old VMs and save report
